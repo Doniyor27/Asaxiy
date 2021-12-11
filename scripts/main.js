62e6574e8f5777f29bab0acaef32575d061088a3
FastDelivery1 <doni404error@gmail.com>
2021-12-11 23:03:52 +0500

    finally

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: false,
+    centerPadding: '60px',
+    
+  });
+});
+
+
+
